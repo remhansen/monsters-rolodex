dacd0bd18be51d010ec072058a235edb42da49e5
Bob Hansen <bob.hansen@hcit.nl>
2020-05-16 13:36:40 +0200

    Updates

---
+++ precache-manifest.c26ee31281d2c0ad2e08d928860909ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96fc9b03dcbd88f69c0f230a44da648",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1538c8697b4867f5f181",
+    "url": "/monsters-rolodex/static/css/main.cc4774b3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1538c8697b4867f5f181",
+    "url": "/monsters-rolodex/static/js/main.b2303d65.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
